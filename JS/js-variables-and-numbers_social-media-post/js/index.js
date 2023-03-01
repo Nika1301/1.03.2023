1b682f8f2c124ca873017e6cda17e0dfc4496cc9
Veronika Popova <freebird130188@gmail.com>
2023-03-01 21:39:40 +0100

    Add folders with tasks from 1.03.23

---
+++ JS/js-variables-and-numbers_social-media-post/js/index.js
@@ -0,0 +1,32 @@
+console.clear();
+
+/*
+1. Create the data for a small social media post. Create a const variable for each data point listed below.:
+	- a title
+	- a text
+	- the number of likes
+	- the user who created the post
+	- a boolean called isReported
+*/
+
+// --v-- write your code here --v--
+const titlePost = "Title";
+const textPost = "text";
+const numberOflikes = 5;
+const userCreatedthePost = 1;
+let isReported = true;
+
+// --^-- write your code here --^--
+
+/*
+2. Log all variables to the console. Increase the likes by one and log the amount of likes again. Adapt your code from 1.1 if necessary.
+*/
+console.log(titlePost);
+console.log(textPost);
+console.log(numberOflikes);
+console.log(userCreatedthePost);
+console.log(isReported);
+
+// --v-- write your code here --v--
+
+// --^-- write your code here --^--
